3994ff2e40f2f234d48ddcd452c0e4d37024cb20
Rikki Malhotra <rikki.malh@gmail.com>
2017-08-03 17:47:42 -0500

    create todo_list and update todo list spec

---
+++ ruby/list/todo_list.rb
@@ -0,0 +1,21 @@
+class TodoList
+
+def initialize(list)
+  @list = list
+end
+
+def get_items
+	@list
+end
+
+def add_item(mop)
+	@list << mop
+
+end
+def del_item
+	
+end
+def get_item
+	
+end
+end
