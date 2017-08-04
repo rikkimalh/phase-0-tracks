a2606dee472871d033108d5839115add81213831
Rikki Malhotra <rikki.malh@gmail.com>
2017-08-04 15:21:09 -0500

    Add getter and setter methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,56 +1,47 @@
 class Santa
-
-
+	attr_reader :age, :ethnicity
+	attr_accessor :gender 
 
 
 	def initialize(gender, ethnicity)
-		p "Initializing Santa Instance..."
+		
 		@gender = gender
 		@ethnicity = ethnicity
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-	end
+			p "Initializing Santa Instance..."
+end
 
 	def speak
-		puts "Ho, Ho, Ho! Haaaaapy Holidays!"
-		puts "Santa is #{@ethnicity}"
-	puts "Santa is #{@gender}"
+			puts "Ho, Ho, Ho! Haaaaapy Holidays!"
+			puts "Santa is #{@ethnicity}"
+			puts "Santa is #{@gender}"
 	end 
 
 	def eat_milk_and_cookies(cookie)
-		puts "That was a good #{cookie}!!"
+			puts "That was a good #{cookie}!!"
 		
 	end 
 	
 	def age1(age)
 		@age1= age
-		puts "Santa is #{age} years old"
+			puts "Santa is #{age} years old"
 	end
 
 	def celebrate_birthday
 		@age = @age1 += 1
-		puts "Happy Birthday! You're now #{@age} years old!"
+			puts "Happy Birthday! You're now #{@age} years old!"
 	end
 
 
 	def get_mad_at(bad_reindeer)
 		@reindeer_ranking.delete_if {|reindeer| reindeer == bad_reindeer}
 		@reindeer_ranking.insert(-1, bad_reindeer)
-		puts "#{bad_reindeer}"
+			puts "#{bad_reindeer}"
 
 	end
-
-	def gender=(new_gender)
-		@gender = new_gender
-	end
-
-	def age
-		@age
-	end 
-
-	def ethnicity
-		@ethnicity
-	end
+	
 end 
+
 
 
 
@@ -66,10 +57,7 @@ santa.celebrate_birthday
 santa.get_mad_at('Dasher')
 
 santa.gender = "Santa has turned into a dragon!"
+puts "#{santa.age}"
 
 puts "#{santa.gender}."
 
-
-#object = Santa.new
-#object.speak
-#object.eat_milk_and_cookies("chocolate Chip")
