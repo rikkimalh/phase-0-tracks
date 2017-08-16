f86cb179afc7144dfc14f4e56e02985f615e0c6c
Rikki Malhotra <rikki.malh@gmail.com>
2017-08-15 22:57:54 -0500

    create algos.js and update code

---
+++ js/algos.js
@@ -0,0 +1,12 @@
+// create an array of words (driver code)
+//break each word into a seperate values in array
+//measure the length of each value. convert string to integer
+//convert the value to the integer equivelant in length (so value = value.length)
+// return the longest value length as its original value (as a string)
+//end 
+
+
+function word_length(str){ 
+		var 
+}
+
